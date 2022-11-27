81be0ff7a26519f32bc51d1384919c33ad3cb1f0
ramji <ramjinmr2001@gmail.com>
2022-11-28 00:40:42 +0530

    update ex

---
+++ AirlineWebApi/ex.cs
@@ -0,0 +1,20 @@
+﻿using AirlineWebApi.Utils;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace AirlineWebApi
+{
+    public class ex
+    {
+        public Type ExceptionType { get; set; }
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IncludeExceptionMessage { get; set; } = true;
+
+        private ILogger<ExceptionMapperAttribute> logger;
+
+    }
+}
